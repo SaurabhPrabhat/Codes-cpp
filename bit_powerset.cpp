26842dfb322bcb44de7fd040d710808ee7a4f747
SaurabhPrabhat <124090506+SaurabhPrabhat@users.noreply.github.com>
2023-07-06 21:06:12 +0530

    Create bit_powerset.cpp
    Finding powerset of a set/array using & operation.

---
+++ bit_powerset.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int a[]={3,4,1};
+    int n=3;
+    
+    for(int i=0;i<=(1<<n)-1;i++){
+        vector<int>v;
+        for(int j=0;j<n;j++){
+            if(1&(i>>j))v.push_back(a[j]);
+        }
+        for(auto k:v)cout<<k<<" ";cout<<endl;
+    }
+}

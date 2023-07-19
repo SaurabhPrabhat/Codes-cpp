cdec4cf9dcccf687c56cf4e43e823c995b53b740
SaurabhPrabhat <124090506+SaurabhPrabhat@users.noreply.github.com>
2023-07-19 15:26:56 +0530

    find_totalno_of_oddlengthsquaresinchessboard.cpp

--- cf_chefandboardgames.cpp
+++ cf_chefandboardgames.cpp
@@ -13,10 +13,12 @@ using namespace std;
 
 
 void solve(){
+   // here  n is the size of chessboard
    int n;
    cin>>n;
    int ans=0;
    for(int i=1;i<=n;i++){
+      // adding number of squares of odd length each odd length 'i' between from 1 to n 
    	if(i&1==1){
    	ans+=(n-i+1)+(n-i+1)*(n-i);}}
    	cout<<ans<<endl;

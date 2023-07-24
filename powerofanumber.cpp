d755307c3e3e52b8b74a6c5411d8ab95d22b834c
SaurabhPrabhat <124090506+SaurabhPrabhat@users.noreply.github.com>
2023-07-24 15:26:06 +0530

    Update powerofanumber.cpp

--- powerofanumber.cpp
+++ powerofanumber.cpp
@@ -1,3 +1,4 @@
+//// recursive  o(logn)
 class Solution {
 public:
     double myPow(double x, int n) {
@@ -13,4 +14,36 @@ public:
       
         return res*res;
             }
+};
+
+//////iterative o(n)
+class Solution {
+public:
+    double myPow(double x, long long int n) {
+      if(n==0||x==1)return 1;
+   
+      else if(n<0){
+          x=1/x;
+          n=-n;
+      } 
+      double ans=1;
+      int t=0;
+      ans*=x;
+      n--;
+      while(n){
+          t++;
+          if(t<n){
+              ans*=ans;
+              n-=t;
+              t+=t;
+          }
+          if(n>0){
+              ans*=x;
+              n--;
+             
+          }
+      }
+      return ans;
+
+    }
 };
